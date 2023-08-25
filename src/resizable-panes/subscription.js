8672616ff1783f6316fffda863226e5adf89dbeb
vimalsharmazones <63034391+vimalsharmazones@users.noreply.github.com>
2023-08-25 22:00:07 +0530

    Subscription init

---
+++ src/resizable-panes/subscription.js
@@ -0,0 +1,22 @@
+
+
+
+
+class Subscription {
+
+    subscriptionList = {}
+
+    subscribe(id, event) {
+         this.subscriptionList[id] = event
+    }
+
+    publish(id, value){
+        this.subscriptionList[id](value)
+    }
+
+    unSubscribe(id){
+        delete this.subscriptionList[id]
+    }
+}
+
+export const subscription = new Subscription
