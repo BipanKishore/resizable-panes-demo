397b8855166ae3b8c9d7979972c51a41c631f96b
vimalsharmazones <63034391+vimalsharmazones@users.noreply.github.com>
2023-09-12 10:08:54 +0530

    export * for type script

--- packages/react-resizable-panes/src/@types/index.ts
+++ packages/react-resizable-panes/src/@types/index.ts
@@ -1,22 +1,4 @@
-export {
-  onResizeType,
-  IResizablePanesProps,
-  IPane
-} from './component-types'
-export {
-  IKeyToBoolMap,
-  IMapIdToSize,
-  IAnyMap,
-  IResizableEvent,
-  SplitType
-} from './general-type'
-export {
-  IResizableApi,
-  IServiceRef,
-  IUseResizablePanesParams,
-  IInitPaneService
-} from './use-resizable-panes-types'
-
-export {
-  IJoinClassNameParam
-} from './utils-type'
+export * from './component-types'
+export * from './general-type'
+export * from './use-resizable-panes-types'
+export * from './utils-type'
