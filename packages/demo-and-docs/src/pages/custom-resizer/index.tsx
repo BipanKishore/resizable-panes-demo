4a78f5bbbccb9819caf3c37674d53fe303b209be
vimalsharmazones <63034391+vimalsharmazones@users.noreply.github.com>
2023-09-20 09:39:05 +0530

    CustomResizer

--- packages/demo-and-docs/src/pages/custom-resizer/index.tsx
+++ packages/demo-and-docs/src/pages/custom-resizer/index.tsx
@@ -5,6 +5,8 @@ import {
 import MarkdownPreview from '@uiw/react-markdown-preview'
 import {CustomResizerFirst} from './custom-resizer-first'
 import {CustomResizerSecond} from './custom-resizer-second'
+import CUSTOM_RESIZER_MD from './custom-resizer.md'
+
 export const CustomResizer = () => {
   const pane1 = 'pane1'
   const pane2 = 'pane2'
@@ -12,21 +14,16 @@ export const CustomResizer = () => {
   return (
     <div>
       <div>
-        <h3 className='t-color-mainBlue t-aligin-center'>Vertical Panes</h3>
+        <h3 className='t-color-mainBlue t-aligin-center'>Custom Resizer for Panes</h3>
       </div>
 
       <div className='m-20-0'>
         To align panes vertical just pass "vertical" as split prop for ResizablePanes.
       </div>
       <div className='m-20-0' >
-        <ResizablePanes
-          className='h-300'
-          split='vertical'
-        >
-
+        <ResizablePanes className='h-300' split='vertical'>
           <Panes
-            className={pane1}
-            id={pane1}
+            className={pane1} id={pane1}
             resizer={
               <CustomResizerFirst />
             }
@@ -35,27 +32,22 @@ export const CustomResizer = () => {
           </Panes>
 
           <Panes
-            className={pane2}
-            id={pane2}
+            className={pane2} id={pane2}
             resizer={
               <CustomResizerSecond />
-              }
+            }
             size={300}
           >
           </Panes>
 
-          <Panes
-            className={pane3}
-            id={pane3}
-            size={150}
-          >
+          <Panes className={pane3} id={pane3} size={150}>
           </Panes>
 
         </ResizablePanes>
       </div>
 
       <div className="mark-down-container m-20-0">
-        <MarkdownPreview className="" source={''} />
+        <MarkdownPreview className="" source={CUSTOM_RESIZER_MD} />
       </div>
     </div>
   )
