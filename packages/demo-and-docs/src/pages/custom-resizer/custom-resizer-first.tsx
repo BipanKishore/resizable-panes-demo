25315460eeae2907846763fcdb62b773d653c4d1
Sandeepg067 <Sandeepg067@gmail.com>
2023-09-26 15:38:50 -0400

    all-style changes

--- packages/demo-and-docs/src/pages/custom-resizer/custom-resizer-first.tsx
+++ packages/demo-and-docs/src/pages/custom-resizer/custom-resizer-first.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import resize_tool_horizontal from "../../../assets/icons/resize_tool_horizontal.svg"
 
 export const CustomResizerFirst = () => {
   return (
     <div
+      className='crhover'
       style={{
         width: '15px'
       }}
     >
+
+
     </div>
   )
 }
