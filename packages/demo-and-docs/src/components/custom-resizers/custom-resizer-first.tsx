f34927b47d85ef48c58ac1d3cdc2ce2bd5298c2a
BipanKishore <bipankishore@gmail.com>
2023-09-28 01:40:39 +0530

    onMouseDown for custom resizer

--- packages/demo-and-docs/src/components/custom-resizers/custom-resizer-first.tsx
+++ packages/demo-and-docs/src/components/custom-resizers/custom-resizer-first.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import {joinClassName} from '../../shared/utils'
 
-export const CustomResizerFirst = ({horizontal}: {
-  horizontal?: boolean
+export const CustomResizerFirst = ({horizontal, onMouseDown}: {
+  horizontal?: boolean,
+  onMouseDown?:any
 }) => {
   const parentClassName = joinClassName({
     'custom-resizer-1st-parent': true,
-    'h-100p w-12 vertical-cursur flex-column': !horizontal,
-    'h-12 horizontal-cursur flex-row': horizontal
+    'h-100p w-12 flex-column': !horizontal,
+    'h-12  flex-row': horizontal
   })
 
   const childClassName = joinClassName({
     'custom-resizer-1st-child': true,
-    'h-70p w-100p': !horizontal,
-    'w-70p h-100p': horizontal
+    'h-70p w-100p vertical-cursur': !horizontal,
+    'w-70p h-100p horizontal-cursur': horizontal
   })
 
   return (
     <div className={parentClassName}>
-      <div className={childClassName}></div>
+      <div className={childClassName} onMouseDown={onMouseDown}></div>
     </div>
   )
 }
