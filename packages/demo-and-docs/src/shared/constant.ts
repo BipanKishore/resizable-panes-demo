da8757b700e0f604480057177120b3628c60ff65
BipanKishore <bipankishore@gmail.com>
2023-09-28 12:25:21 +0530

    re-ordered tabs

--- packages/demo-and-docs/src/shared/constant.ts
+++ packages/demo-and-docs/src/shared/constant.ts
@@ -7,10 +7,10 @@ export const ROUTER_LIST_LEVEL_1 = [
   {label: 'Horizontal Panes', path: 'horizontal-panes'},
   {label: 'Min & Max Sizes', path: 'min-n-max'},
   {label: 'Custom Resizer', path: 'custom-resizer'},
-  {label: 'Show & Hide Panes', path: 'show-n-hide'},
   {label: 'Nested Panes', path: 'nested-panes'},
   {label: 'Full screen modes', path: 'full-screen-modes'},
   {label: 'ResizablePanes Props', path: 'resizable-panes-props'},
-  {label: 'Pane Props', path: 'pane-props'}
+  {label: 'Pane Props', path: 'pane-props'},
+  {label: 'Show & Hide Panes', path: 'show-n-hide'}
   // {label: 'Api Description', path: 'api-docs'}
 ]
