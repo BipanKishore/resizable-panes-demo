9a0c98649cb80a0bc66c23da08ad99489f42bab7
BipanKishore <bipankishore@gmail.com>
2023-09-30 15:13:22 +0530

    Implementation custom resizer

---
+++ packages/demo-and-docs/src/pages/custom-resizer/custom-resizer-implementation.md
@@ -0,0 +1,31 @@
+
+
+```jsx mdx:preview
+
+interface ICustomResizerProp {
+    onMouseDown:any,
+    isMouseDown: boolean,
+    onTouchStartCapture: any
+  }
+
+
+export const CustomResizer = ({
+    onMouseDown,
+    isMouseDown, 
+    onTouchStartCapture
+}: ICustomResizerProp) => {
+
+ // isMouseDown: use it to style the elements
+
+  return (
+    <SOME_ELEMENT>
+      <TARGET_ELEMENT
+        onMouseDown={onMouseDown}
+        onTouchStartCapture={onTouchStartCapture}
+      >
+      </TARGET_ELEMENT>
+    </SOME_ELEMENT>
+  )
+}
+
+```
--- packages/demo-and-docs/src/pages/custom-resizer/index.tsx
+++ packages/demo-and-docs/src/pages/custom-resizer/index.tsx
@@ -4,6 +4,7 @@ import {
 } from 'resizable-panes-react'
 import MarkdownPreview from '@uiw/react-markdown-preview'
 import CUSTOM_RESIZER_MD from './custom-resizer.md'
+import CUSTOM_RESIZER_IMPLIMENTATION from './custom-resizer-implementation.md'
 import {CustomResizerFirst} from '../../components/custom-resizers/custom-resizer-first'
 import {CustomResizerSecond} from '../../components/custom-resizers/custom-resizer-second'
 
@@ -14,7 +15,7 @@ export const CustomResizer = () => {
   return (
     <div>
       <div>
-        <h3 className='t-color-mainBlue t-aligin-center'>Custom Resizer for Panes</h3>
+        <h3 className='t-aligin-center'>Custom Resizer for Panes</h3>
       </div>
 
       <div className='m-20-0'>
@@ -22,17 +23,7 @@ export const CustomResizer = () => {
           to the <code>ResizablePanes</code> component, it affects all panes. Alternatively, you can use
           the <code>Pane</code> component's <code>resizer</code> prop to customize the resizer for a specific pane.
         </p>
-        <p>"The <code>CustomResizer</code> component should include
-          an <code>onMouseDown</code> (and <code>onTouchStartCapture </code>
-          for touch devices)
-          prop, which should
-          be attached to the element that, upon being clicked and dragged, initiates the resizing of the pane's size."
-        </p>
-        <p>The <code>CustomResizer</code> component will have
-          a <code>isMouseDown</code> boolean prop, which you can utilize to
-          style the <code>CustomResizer</code> component based on whether a mouse button
-          is being held down during resizing.
-        </p>
+
       </div>
       <div className='m-20-0' >
         <ResizablePanes
@@ -66,6 +57,28 @@ export const CustomResizer = () => {
       <div className="mark-down-container m-20-0">
         <MarkdownPreview className="" source={CUSTOM_RESIZER_MD} />
       </div>
+
+      <div>
+        <h4 className=''>Implementation</h4>
+      </div>
+      <div className="m-20-0">
+        <p>"The <code>CustomResizer</code> component should include
+          an <code>onMouseDown</code> (and <code>onTouchStartCapture </code>
+          for touch devices)
+          prop, which should
+          be attached to the element that, upon being clicked and dragged, initiates the resizing of the pane's size."
+        </p>
+        <p>The <code>CustomResizer</code> component will have
+          a <code>isMouseDown</code> boolean prop, which you can utilize to
+          style the <code>CustomResizer</code> component based on whether a mouse button
+          is being held down during resizing.
+        </p>
+      </div>
+
+      <div className="mark-down-container m-20-0">
+        <MarkdownPreview className="" source={CUSTOM_RESIZER_IMPLIMENTATION} />
+      </div>
+
     </div>
   )
 }
